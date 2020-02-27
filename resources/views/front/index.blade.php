3145e255e05f8a426a3a5b5620c35001c7479379
zzz9898s10022 <61447866+zzz9898s10022@users.noreply.github.com>
2020-02-27 11:28:29 +0800

    first update

---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layout/nav')
+
+
+@section('content')
+
+
+<section class="engine"><a href="https://mobirise.info/g">build a website for free</a></section><section class="cid-qTkA127IK8 mbr-fullscreen mbr-parallax-background" id="header2-1">
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">
+                    FULL SCREEN INTRO
+                </h1>
+
+                <p class="mbr-text pb-3 mbr-fonts-style display-5">
+                    Click any text to edit or style it. Select text to insert a link. Click blue "Gear" icon in the top right corner to hide/show buttons, text, title and change the block background. Click red "+" in the bottom right corner to add a new block. Use the top left menu to create new pages, sites and add themes.
+                </p>
+                <div class="mbr-section-btn">
+                    <a class="btn btn-md btn-secondary display-4" href="/">LEARN MORE</a>
+                    <a class="btn btn-md btn-white-outline display-4" href="/">LIVE DEMO</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
+
+<section class="mbr-gallery mbr-slider-carousel cid-rRF176nNHj" id="gallery1-3">
+    <div class="container">
+        <div><!-- Filter --><!-- Gallery --><div class="mbr-gallery-row"><div class="mbr-gallery-layout-default"><div><div><div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="酷毙"><div href="#lb-gallery1-3" data-slide-to="0" data-toggle="modal"><img src="assets/images/background1.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">这里输入标题</span></div></div><div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="响应式"><div href="#lb-gallery1-3" data-slide-to="1" data-toggle="modal"><img src="assets/images/background2.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">这里输入标题</span></div></div><div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="创意"><div href="#lb-gallery1-3" data-slide-to="2" data-toggle="modal"><img src="assets/images/background3.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">这里输入标题</span></div></div><div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="动画"><div href="#lb-gallery1-3" data-slide-to="3" data-toggle="modal"><img src="assets/images/background4.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">这里输入标题</span></div></div><div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="酷毙"><div href="#lb-gallery1-3" data-slide-to="4" data-toggle="modal"><img src="assets/images/background5.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">这里输入标题</span></div></div><div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="酷毙"><div href="#lb-gallery1-3" data-slide-to="5" data-toggle="modal"><img src="assets/images/background6.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">这里输入标题</span></div></div><div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="响应式"><div href="#lb-gallery1-3" data-slide-to="6" data-toggle="modal"><img src="assets/images/background7.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">这里输入标题</span></div></div><div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="动画"><div href="#lb-gallery1-3" data-slide-to="7" data-toggle="modal"><img src="assets/images/background8.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">这里输入标题</span></div></div></div></div><div class="clearfix"></div></div></div><!-- Lightbox --><div data-app-prevent-settings="" class="mbr-slider modal fade carousel slide" tabindex="-1" data-keyboard="true" data-interval="false" id="lb-gallery1-3"><div class="modal-dialog"><div class="modal-content"><div class="modal-body"><div class="carousel-inner"><div class="carousel-item active"><img src="assets/images/background1.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background2.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background3.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background4.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background5.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background6.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background7.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background8.jpg" alt="" title=""></div></div><a class="carousel-control carousel-control-prev" role="button" data-slide="prev" href="#lb-gallery1-3"><span class="mbri-left mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control carousel-control-next" role="button" data-slide="next" href="#lb-gallery1-3"><span class="mbri-right mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Next</span></a><a class="close" href="#" role="button" data-dismiss="modal"><span class="sr-only">Close</span></a></div></div></div></div></div>
+    </div>
+
+</section>
+
+<section class="mbr-section article content11 cid-rRF18vYb9Z" id="content11-4">
+
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="mbr-text counter-container col-12 col-md-8 mbr-fonts-style display-7">
+                <ol>
+                    <li><strong>MOBILE FRIENDLY</strong> - no special actions required, all sites you make with Mobirise are mobile-friendly. You don't have to create a special mobile version of your site, it will adapt automagically. <a href="https://mobirise.co/">Try it now!</a></li>
+                    <li><strong>EASY AND SIMPLE</strong> - cut down the development time with drag-and-drop website builder. Drop the blocks into the page, edit content inline and publish - no technical skills required. <a href="https://mobirise.co/">Try it now!</a></li>
+                    <li><strong>UNIQUE STYLES</strong> - choose from the large selection of latest pre-made blocks - full-screen intro, bootstrap carousel, content slider, responsive image gallery with lightbox, parallax scrolling, video backgrounds, hamburger menu, sticky header and more. <a href="https://mobirise.co/">Try it now!</a></li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section article content14 cid-rRF19x19Q9" id="content14-5">
+
+
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="row col-12 col-md-12">
+                <div class="col-12 mbr-text mbr-fonts-style col-md-4 display-7">
+                     <p class="p-3">Make your own website in a few clicks! Mobirise helps you cut down development time by providing you with a flexible website editor with a drag and drop interface. Mobirise Website Builder creates responsive, retina and mobile friendly websites in a few clicks.</p>
+                </div>
+                <div class="col-12 mbr-text mbr-fonts-style display-7 col-md-4">
+                     <p class="p-3">Make your own website in a few clicks! Mobirise helps you cut down development time by providing you with a flexible website editor with a drag and drop interface. Mobirise Website Builder creates responsive, retina and mobile friendly websites in a few clicks.</p>
+                </div>
+                <div class="col-12 col-md-4 mbr-text mbr-fonts-style display-7">
+                     <p class="p-3">Make your own website in a few clicks! Mobirise helps you cut down development time by providing you with a flexible website editor with a drag and drop interface. Mobirise Website Builder creates responsive, retina and mobile friendly websites in a few clicks.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
